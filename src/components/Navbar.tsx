2ec3cad6597dc32b2c09e5b11b7433b98dade21f
Phajsi Choque Halvorsen <phajsich@stud.ntnu.no>
2020-11-16 00:26:07 +0100

    adding comments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import logo from "../assets/switchDB_logoV3.png";
 import styles from '../styles/NavbarStyles';
 
 const NavBar = () => (
+  //shows the title and logo of the application
   <View style={styles.navBar}>
     <Image style={styles.navLogo} source={logo} />
     <Text style={styles.navText}> nintendo games db</Text>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Item, Input, Icon, Header} from "native-base";
 
 const SearchBar = ({setSearch}) => (
-
+  //updates the search state when text is typed into the search bar
   <Header searchBar style={{backgroundColor: 'red'}}>
   <Item>
     <Icon name="ios-search" />
